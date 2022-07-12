064b0c36723769c6d9f4411ed9ce4cc5b0d1caf6
LAPTOP-LHUOQ7VL\91700 <ranjan.intel@gmail.com>
2022-07-12 17:31:08 +0530

    function

---
+++ JavaFunction.java
@@ -0,0 +1,13 @@
+public class JavaFunction {
+    public static void main(String[] args) {
+        //calling a function
+        additon(10,5);
+    }
+
+    //function body
+    public static void additon(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
+}
+
